3fad246ca87bfbb3543c4830284ffecef3678336
zebang.eth <zebang.li.2012@gmail.com>
2024-03-23 13:25:19 -0400

    Added default possible ingredients

--- server/setupMongo.ts
+++ server/setupMongo.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb'
-import { Operator, Customer } from './data'
+import { Operator, Customer, Ingredient } from './data'
 
 // Connection URL
 const url = 'mongodb://127.0.0.1:27017'
@@ -27,21 +27,36 @@ const customers: Customer[] = [
   },
 ]
 
+// New default possible ingredients
+const possibleIngredients: Ingredient[] = [
+  { _id: "i1", name: "Strawberry", cost: 1 },
+  { _id: "i2", name: "Banana", cost: 0.8 },
+  { _id: "i3", name: "Mango", cost: 1.2 },
+  { _id: "i4", name: "Spinach", cost: 0.5 },
+  { _id: "i5", name: "Kale", cost: 0.5 },
+  { _id: "i6", name: "Blueberry", cost: 1 },
+  { _id: "i7", name: "Almond Milk", cost: 0.6 },
+  { _id: "i8", name: "Oat Milk", cost: 0.4 },
+  { _id: "i9", name: "Greek Yogurt", cost: 0.9 },
+  { _id: "i10", name: "Honey", cost: 0.7 },
+]
+
 async function main() {
   await client.connect()
   console.log('Connected successfully to MongoDB')
 
   const db = client.db("test")
 
-  // set up unique index for upsert -- to make sure a customer cannot have more than one draft order
   db.collection("orders").createIndex(
     { customerId: 1 }, 
     { unique: true, partialFilterExpression: { state: "draft" } }
   )
 
-  // add data
   console.log("inserting customers", await db.collection("customers").insertMany(customers as any))
   console.log("inserting operators", await db.collection("operators").insertMany(operators as any))
+  
+  // Insert possible ingredients
+  console.log("inserting possible ingredients", await db.collection("possibleIngredients").insertMany(possibleIngredients as any))
 
   process.exit(0)
 }
